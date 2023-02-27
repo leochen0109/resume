39052f5ce690901bc5431e116b78d4c2237512ed
leochen0109 <nichen@ucsd.edu>
2023-02-27 00:33:42 -0800

    :tada: Initial commit

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <h1>Resume</h1>
+  <el-icon>
+    <i-ep-plus></i-ep-plus>
+  </el-icon>
+</template>
+<script setup>
+
+</script>
+<script>
+export default {
+  name: "App"
+}
+</script>
+
+<style scoped lang="less">
+
+</style>

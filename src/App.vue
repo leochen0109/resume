9f7aee6210344b82c61a8e4b3c498a70d9f606c0
leochen0109 <nichen@ucsd.edu>
2023-02-27 01:15:32 -0800

    :+1: change name of image

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,9 @@
 <template>
-  <h1>Niehao Chen<br>
+  <div>Niehao Chen<br>
     4062 La Jolla Village Dr, La Jolla,CA,92037 | (858)214-0609<br>
     www.linkedin.com/in/niehao-chen
-  </h1>
-  <div class="demo-image__lazy">
+  </div>
+  <div class="image">
     <el-image src="src/pictures/resume pic.png" />
   </div>
 </template>
